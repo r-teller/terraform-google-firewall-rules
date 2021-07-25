566a7d1937b4b9f0a31894acdd6558cd2a3863ed
Robert Teller <robert.j.c.teller@gmail.com>
2021-07-25 14:04:00 -0700

    update

--- variables.tf
+++ variables.tf
@@ -8,4 +8,14 @@ variable "project_id" {
 
 variable "firewall_rule" {
     description = "Firewall rule that need to be created"
+    type=object({
+        id          = string,
+        description = string,
+        action      = string,
+        direction   = string,
+        log_config  = string,
+        priority    = number,
+        sources     = list(string),
+        targets     = list(string),
+    })
 }
