e275fe6d3d901e215d26ab0b347dfcaf69ee9328
Robert Teller <Robert.J.C.Teller@Gmail.com>
2024-03-02 00:19:29 -0800

    updating UI

--- build_docs_v2/src/App.js
+++ build_docs_v2/src/App.js
@@ -9,7 +9,7 @@ import { ArrayFieldTemplateProps, DescriptionFieldProps, RJSFSchema } from '@rjs
 import firewallRulesRawSchema from './schemas/resolved.schema.json';
 import firewallRulesUISchema from './schemas/UISchema.json';
 import Ajv from 'ajv';
-import ajvErrors from 'ajv-errors';
+// import ajvErrors from 'ajv-errors';
 
 import validator from '@rjsf/validator-ajv8';
 // import './App.css'; // Assuming your custom CSS is here
@@ -34,10 +34,10 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 // import { ArrayFieldTemplateProps } from '@rjsf/utils';
 
-// Initialize AJV with options suitable for your needs
-const ajv = new Ajv({ allErrors: true, useDefaults: true });
-ajvErrors(ajv); // Optional: Use ajv-errors for better error messages
-
+// // Initialize AJV with options suitable for your needs
+const ajv = new Ajv({ strict: false, allErrors: true, useDefaults: true });
+// ajvErrors(ajv); // Optional: Use ajv-errors for better error messages
+const validate = ajv.compile(firewallRulesRawSchema)
 const schema = {
   type: "object",
   properties: {
@@ -45,24 +45,43 @@ const schema = {
   }
 }
 
+function CustomFieldTemplate(props) {
+  const { id, classNames, label, help, required, description, errors, children } = props;
+  return (
+    <div className={classNames}>
+      winner
+      <label htmlFor={id}>{label}{required ? "A" : null}</label>
+      {description}
+      {children}
+      {errors}
+      {help}
+    </div>
+  );
+}
+
 
 const uiSchema = {
-  // 'firewall_rules': firewallRulesUISchema,
+  'firewall_rules': firewallRulesUISchema,
   'ui:submitButtonOptions': {
     norender: true
   },
-  'firewall_rules': {
-    // 'ui:ArrayFieldItemTemplate': ArrayFieldItemTemplate,
-    'ui:ArrayFieldTemplate': ArrayFieldTemplate,
-    "ui:options": {
-      "copyable": true
-    },
-    'items': {
-      'direction': {
-        'ui:DescriptionFieldTemplate': DescriptionFieldTemplate
-      }
-    }
-  }
+  // 'firewall_rules': {
+  //   // 'ui:ArrayFieldTemplate': CustomFieldTemplate,
+  //   items: {
+  //     direction: {
+  //       // 'ui:FieldTemplate': CustomFieldTemplate,
+  //     }
+  //   }
+  //   //   // 'ui:ArrayFieldItemTemplate': ArrayFieldItemTemplate,
+  //   //   "ui:options": {
+  //   //     "copyable": true
+  //   //   },
+  //   //   'items': {
+  //   //     'direction': {
+  //   //       'ui:DescriptionFieldTemplate': DescriptionFieldTemplate
+  //   //     }
+  //   //   }
+  // }
 }
 
 function ArrayFieldTemplate(props) {
@@ -70,10 +89,14 @@ function ArrayFieldTemplate(props) {
   return (
     <div>
       {props.items.map((element) => element.children)}
-      {props.canAdd && <button type='button' onClick={props.onAddClick} 
-      // class='btn btn-info btn-add col-xs-12' 
-      className={className}
-      title='Add'>Add </button>}
+      {props.canAdd && <button type='button' onClick={props.onAddClick}
+        // class='btn btn-info btn-add col-xs-12' 
+        className={className}
+        title='Add'>Add </button>}
+      {props.hasRemove && <button type='button' onClick={props.onAddClick}
+        // class='btn btn-info btn-add col-xs-12' 
+        className={className}
+        title='Add'>Remove </button>}
       {/* {props.canDe && <button type='button' onClick={props.onAddClick}></button>} */}
     </div>
   );
@@ -108,6 +131,13 @@ function DescriptionFieldTemplate(props) {
   );
 }
 
+function formatErrors(errors) {
+  return errors
+    .filter(error => error.keyword !== 'const' && error.keyword !== 'x') // Exclude errors with 'const' or 'x'
+    .map(error => `${error.instancePath}, ${error.schemaPath}, ${error.message}`)
+    .join('\n'); // Join errors with newline for title attribute usage
+}
+
 
 // Define a function to handle form submission
 const onSubmit = ({ formData }, e) => console.log("Data submitted: ", formData);
@@ -115,6 +145,9 @@ const onSubmit = ({ formData }, e) => console.log("Data submitted: ", formData);
 function App() {
   const [formData, setFormData] = useState({}); // Initial form data
   const [showModal, setShowModal] = useState(false);
+  const [isInputValid, setIsInputValid] = useState(true);
+  const [modalInputData, setModalInputData] = useState("");
+  const [errorTitle, setErrorTitle] = useState('');
 
   const resetFormData = () => {
     setFormData({}); // Reset form data to initial state, adjust { } as needed
@@ -132,35 +165,63 @@ function App() {
 
   const loadJsonFromClipboard = async () => {
     try {
-      const text = await navigator.clipboard.readText(); // Read text from clipboard
-      const formData = JSON.parse(text); // Attempt to parse it as JSON
+      // const text = await navigator.clipboard.readText(); // Read text from clipboard
+      // const formData = JSON.parse(text); // Attempt to parse it as JSON
+      const formData = JSON.parse(modalInputData)
       setFormData({ firewall_rules: formData }); // Update your form's state
       console.log('FormData loaded from clipboard');
+      toggleModal();
     } catch (err) {
       console.error('Failed to load JSON from clipboard:', err);
     }
   };
 
+  // Used to track state for main form
   const handleFormDataChange = ({ formData }) => {
     setFormData(formData);
   };
+
+  const handleModalInputChange = (e) => {
+    // console.log("Button")
+    try {
+      const inputData = JSON.parse(e.target.value);
+      const valid = validate(inputData);
+      setIsInputValid(valid)
+      if (!valid) {
+        console.log(validate.errors);
+        setErrorTitle(formatErrors(validate.errors));
+      }
+
+    } catch (err) {
+      setIsInputValid(false)
+      setErrorTitle(err)
+      console.error('Failed to load JSON from input:', err);
+    }
+    // // Update modal input data without affecting formData
+    setModalInputData(e.target.value);
+  };
+
   const toggleModal = () => {
     // console.log("Toggle clicked")
     console.log("Toggling modal from", showModal, "to", !showModal);
     setShowModal(!showModal)
+    if (!showModal) {
+      setModalInputData(JSON.stringify(formData.firewall_rules, null, 2));
+    }
   };
+
   return (
     <div className="App container mt-5">
 
       <center>
-        <button onClick={() => loadJsonFromClipboard(formData)} className="btn btn-info btn-lg" title="Copy to Clipboard">
+        {/* <button onClick={() => loadJsonFromClipboard(formData)} className="btn btn-info btn-lg" title="Paste from Clipboard">
           <FontAwesomeIcon icon={faPaste} /> Load FormData
-        </button>
+        </button> */}
         <button onClick={() => copyJsonToClipboard(formData)} className="btn btn-info btn-lg" title="Copy to Clipboard">
           <FontAwesomeIcon icon={faCopy} /> Copy FormData
         </button>
         <button onClick={toggleModal} className="btn btn-info btn-lg" title="View JSON">
-          <FontAwesomeIcon icon={faEye} /> View JSON
+          <FontAwesomeIcon icon={faEye} /> Edit / View FormData
         </button>
         <button onClick={() => window.open(document.location.pathname + "./documentation", "_blank")} className="btn btn-info btn-lg" title="View Documentation">
           <FontAwesomeIcon icon={faBook} /> View Documentation
@@ -182,11 +243,23 @@ function App() {
                 <h4 className="modal-title">Form Data JSON</h4>
               </div>
               <div className="modal-body">
-                <pre>{JSON.stringify(formData.firewall_rules, null, 2)}</pre>
+                <textarea className="editable-pre"
+                  rows="25" // Adjust the number of rows as needed
+                  cols="50" // Adjust the number of columns as needed
+                  value={modalInputData}
+                  onChange={handleModalInputChange}
+                  style={{ whiteSpace: "pre-wrap" }}
+                // onChange={(e) => console.log(e.target.value)}
+                />
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-default" onClick={() => copyJsonToClipboard(formData)}>Copy JSON</button>
-                <button type="button" className="btn btn-default" onClick={toggleModal}>Close</button>
+                <button type="button"
+                  title={!isInputValid ? errorTitle : ""}
+                  disabled={!isInputValid}
+                  className="btn btn-warning"
+                  onClick={() => loadJsonFromClipboard(modalInputData)}>Load JSON</button>
+                <button type="button" className="btn btn-secondary" onClick={() => copyJsonToClipboard(formData)}>Copy JSON</button>
+                <button type="button" className="btn btn-primary" onClick={toggleModal}>Close</button>
               </div>
             </div>
           </div>
@@ -205,7 +278,12 @@ function App() {
         onChange={handleFormDataChange}
         liveValidate
         validator={validator}
-      // templates={{ DescriptionFieldTemplate }}
+
+        templates={{
+          DescriptionFieldTemplate,
+          // CustomFieldTemplate,
+        }}
+      // FieldTemplate={{ CustomFieldTemplate }}
       // fields={{ ArrayFieldTemplate }}
       />
     </div>
